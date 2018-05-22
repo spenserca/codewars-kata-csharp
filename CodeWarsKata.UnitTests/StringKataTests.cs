1584b1af8d7f8e230d044865d2be09dd4965dfaa
Andreassen <spencer.andreassen@PIONEER.COM>
2018-05-22 16:21:35 -0500

    Initial commit

---
+++ CodeWarsKata.UnitTests/StringKataTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace CodeWarsKata.UnitTests
+{
+    public class StringKataTests
+    {
+        private readonly StringKata stringKata;
+
+        public StringKataTests()
+        {
+            stringKata = new StringKata();
+        }
+
+        [Fact]
+        public void ShouldConvertToCamelCaseWithUnderscores()
+        {
+            Assert.Equal("theStealthWarrior", stringKata.ToCamelCase("the_stealth_warrior"));
+        }
+
+        [Fact]
+        public void ShouldConvertToCamelCaseWithDashes()
+        {
+            Assert.Equal("TheStealthWarrior", stringKata.ToCamelCase("The-Stealth-Warrior"));
+        }
+    }
+}
---
+++ CodeWarsKata/StringKata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace CodeWarsKata
+{
+    public class StringKata
+    {
+        public string ToCamelCase(string s)
+        {
+            var strings = s.Split(new[] { '-', '_' });
+
+            return $"{strings[0]}{UpperCaseFirstChars(strings)}";
+        }
+
+        private string UpperCaseFirstChars(string[] strings)
+        {
+            var sb = new StringBuilder();
+            for (int i = 1; i < strings.Length; i++)
+            {
+                var chars = strings[i].ToCharArray();
+                chars[0] = Char.ToUpper(chars[0]);
+                var s = string.Join("", chars);
+                sb.Append(s);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
